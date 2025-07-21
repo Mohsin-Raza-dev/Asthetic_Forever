d2acbb795040885b0ca005b2cd965f52473cd5e7
Mohsin-Raza-dev <imohsinraza79@gmail.com>
2025-07-21 18:19:39 +0500

    feat(cartTotal): implement subtotal, shipping, and total logic

---
+++ Frontend/src/components/CartTotal.jsx
@@ -0,0 +1,64 @@
+import React, { useContext, useMemo } from "react";
+import Title from "./Title";
+import { ShopContext } from "../context/ShopContext";
+
+const CartTotal = () => {
+  const { cartItems, products, deliver_fee, currency } =
+    useContext(ShopContext);
+
+
+  const subtotal = useMemo(() => {
+    let total = 0;
+    for (let productId in cartItems) {
+      const product = products.find((p) => p._id.toString() === productId);
+      if (product) {
+        for (let size in cartItems[productId]) {
+          total += product.price * cartItems[productId][size];
+        }
+      }
+    }
+    return total;
+  }, [cartItems, products]);
+
+  const shipping = subtotal > 0 ? deliver_fee : 0;
+  const total = subtotal + shipping;
+
+    return subtotal > 0 ? (
+                  <div className="flex flex-col justify-end mt-14 w-1/3">
+      <div className="flex text-xl sm:text-2xl font-semibold">
+        <Title text1="CART" text2="TOTAL" />
+      </div>
+
+      <div className="flex flex-col gap-4 mt-4">
+        <div className="flex justify-between">
+          <p className="text-gray-700 mt-2">Subtotal</p>
+          <p className="text-gray-600 mt-2">
+            {currency}
+            {subtotal.toFixed(2)}
+          </p>
+        </div>
+
+        <div className="flex justify-between border-t border-gray-400">
+          <p className="text-gray-700 mt-2">Shipping</p>
+          <p className="text-gray-600 mt-2">
+            {currency}
+            {shipping.toFixed(2)}
+          </p>
+        </div>
+
+        <div className="flex justify-between border-t border-gray-400">
+          <p className="text-gray-700 font-medium mt-2">Total</p>
+          <p className="text-gray-600 mt-2 font-medium">
+            {currency}
+            {total.toFixed(2)}
+          </p>
+        </div>
+      </div>
+      <button className="mt-6 bg-black text-white py-2 px-6 hover:opacity-90 transition">
+        Proceed to Checkout
+      </button>
+    </div>
+    ) : null;
+};
+
+export default CartTotal;
--- Frontend/src/pages/Cart.jsx
+++ Frontend/src/pages/Cart.jsx
@@ -2,12 +2,16 @@ import {useState, useEffect, useContext} from 'react'
 import Title from '../components/Title'
 import { ShopContext } from '../context/ShopContext'
 import { MdDeleteForever } from "react-icons/md";
+import CartTotal from '../components/CartTotal';
+import { useNavigate } from 'react-router-dom';
 
 
 const Cart = () => {
   const { products, cartItems, currency, setCartItems } = useContext(ShopContext);
 
   const [cartData, setCartData] = useState([]);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
 
@@ -56,66 +60,87 @@ const Cart = () => {
   
   
   return (
-    <div className='border-t pt-14'>
-      <div className='text-2xl mb-3'>
-        <Title text1={"My"} text2={"Cart"} />
-      </div>
-      <div>
-        {
-          cartData.map((item, index) => {
-            const productData = products.find((product) => product._id.toString() === item.id.toString());
+    <div className="border-t pt-14">
+      {cartData.length === 0 ? (
+          <div className="flex flex-col items-center justify-center min-h-[300px] text-center text-gray-500">
+            <p className="text-xl font-semibold mb-2">Your cart is empty</p>
+            <p className="text-sm text-gray-400">
+              Looks like you haven’t added anything yet.
+            </p>
+            <button
+              onClick={() => navigate("/collection")}
+              className="mt-4 px-4 py-2 border border-gray-300 rounded hover:bg-black hover:text-white transition"
+            >
+              Continue Shopping
+            </button>
+          </div>
+      ) : (
+        <>
+          <div className="text-2xl mb-3">
+            <Title text1={"My"} text2={"Cart"} />
+          </div>
+          <div>
+            {cartData.map((item, index) => {
+              const productData = products.find(
+                (product) => product._id.toString() === item.id.toString()
+              );
 
-            return (
-              <div
-                key={index}
-                className="py-4 border-b border-gray-300 text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grod-cols-[4fr_2fr_0.5fr] items-center gap-4"
-              >
+              return (
                 <div
-                  className="flex items-start
-                 gap-6"
+                  key={index}
+                  className="py-4 border-b border-gray-300 text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grod-cols-[4fr_2fr_0.5fr] items-center gap-4"
                 >
-                  <img
-                    src={productData.image[0]}
-                    alt="product"
-                    className="w-16 sm:w-20 object-fit"
-                  />
-                  <div>
-                    <p className="text-sm sm:text-lg font-medium">
-                      {productData.name}
-                    </p>
-                    <div className="flex items-center gap-5 mt-2">
-                      <p>
-                        {currency}
-                        {productData.price}
+                  <div
+                    className="flex items-start
+                 gap-6"
+                  >
+                    <img
+                      src={productData.image[0]}
+                      alt="product"
+                      className="w-16 sm:w-20 object-fit"
+                    />
+                    <div>
+                      <p className="text-sm sm:text-lg font-medium">
+                        {productData.name}
                       </p>
-                      <p className="px-2 sm:px-3 sm:py-1 border border-gray-100 bg-slate-100">
-                        {item.size}
-                      </p>
+                      <div className="flex items-center gap-5 mt-2">
+                        <p>
+                          {currency}
+                          {productData.price}
+                        </p>
+                        <p className="px-2 sm:px-3 sm:py-1 border border-gray-100 bg-slate-100">
+                          {item.size}
+                        </p>
+                      </div>
                     </div>
                   </div>
+                  <div className="flex items-center gap-8">
+                    <input
+                      className="max-w-10 sm:max-w-20 px-1 sm:px-2 py-1 border border-gray-300 bg-gray-50"
+                      type="number"
+                      defaultValue={item.quantity}
+                      min={0}
+                      max={100}
+                      onChange={(e) =>
+                        handleQuantityChange(item.id, item.size, e.target.value)
+                      }
+                    />
+                    <MdDeleteForever
+                      className="cursor-pointer w-4 sm:w-6 h-4 sm:h-6 hover:text-red-500"
+                      onClick={() =>
+                        handleDeleteItem(item.id, item.size, item.quantity)
+                      }
+                    />
+                  </div>
                 </div>
-                <div className="flex items-center gap-8">
-                  <input
-                    className="max-w-10 sm:max-w-20 px-1 sm:px-2 py-1 border border-gray-300 bg-gray-50"
-                    type="number"
-                    defaultValue={item.quantity}
-                    min={0}
-                    max={100}
-                    onChange={(e) =>
-                      handleQuantityChange(item.id, item.size, e.target.value)
-                    }
-                  />
-                  <MdDeleteForever className='cursor-pointer w-4 sm:w-6 h-4 sm:h-6 hover:text-red-500' onClick={() => handleDeleteItem(item.id, item.size, item.quantity)} />
-                </div>
-              </div>
-            );
-          })
-        }
-
-      </div>
-      <div className='text-2xl mb-3 pt-14 flex flex-col flex-end'>
-        <Title text1={"CART"} text2={"TOTAL"} />
-      </div>
+              );
+            })}
+          </div>
+          <div className="flex justify-end mt-10">
+            <CartTotal />
+          </div>
+        </>
+      )}
     </div>
   );
 }
